89bdb2113f2e5b1619b8b7832621dec5fd2d07db
tinhpro248 <69108869+tinhpro248@users.noreply.github.com>
2022-12-15 01:30:25 +0700

    sms

--- src/main/java/com/esme/spring/faircorp/web/UserController.java
+++ src/main/java/com/esme/spring/faircorp/web/UserController.java
@@ -6,12 +6,16 @@ import com.esme.spring.faircorp.Service.UserService;
 import com.esme.spring.faircorp.web.Request.UserRequest;
 import com.esme.spring.faircorp.web.dto.LoginDTO;
 import com.esme.spring.faircorp.web.dto.ProfileDTO;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.Date;
+import java.util.Random;
 
 /**
  * @author devmind
@@ -46,5 +50,18 @@ public class UserController {
     @PostMapping("user/edit-password")
     public boolean editPassword(@Param("id") int id, @RequestHeader String password, @RequestHeader String newPassword){
         return userService.editPassword(id, password, newPassword);
+    }
+
+    @GetMapping(value = "/sendSMS")
+    public String sendSMS(@Param("phone") String phone) {
+
+        Twilio.init("ACe142116383267bf8f8e921941d7b62e0", "fa1cf1ffd0132b3019d6dd22fe0dd266");
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt((999999 - 100000) + 1) + 1000000;
+        Message.creator(new PhoneNumber(String.valueOf(phone)),
+                new PhoneNumber("+15627847500"), "Verify code:" + String.valueOf(randomNum)).create();
+
+        return String.valueOf(randomNum);
     }
 }
